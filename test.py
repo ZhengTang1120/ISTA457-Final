febababaf0ab8b944c523b4d51706157a223c2fa
Zheng Tang <oscartang1120@gmail.com>
2018-03-22 10:18:44 -0700

    0.12

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import utils
 
 if __name__ == '__main__':
     tweets_test, w, c = utils.read_tweets("2018-E-c-En-dev.txt")
-    model, word_index, char_index = utils.load_model("model")
+    model = torch.load("100.model")
+    word_index, char_index = utils.load_model("indices")
     utils.make_vectors_test(tweets_test, word_index, char_index)
     for t in tweets_test:
         print(model(t))
