98e5688efffef5b64dcde12d02ce4c555c11e86f
Zheng Tang <oscartang1120@gmail.com>
2018-03-24 13:21:41 -0700

    1.4

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@ import numpy as np
 
 if __name__ == '__main__':
     tweets_test, w, c = utils.read_tweets("2018-E-c-En-dev.txt")
-    model = torch.load("1.model")
+    model = torch.load("30.model")
     word_index, char_index = utils.load_indices("indices")
     utils.make_vectors_test(tweets_test, word_index, char_index)
     score = 0
