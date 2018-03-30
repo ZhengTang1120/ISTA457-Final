4a14b9277950389bb6ba63f04142070a6c35183b
Zheng Tang <oscartang1120@gmail.com>
2018-03-30 01:31:08 -0700

    ..

--- predict.py
+++ predict.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     tweets_test, w, c = utils.read_tweets("2018-E-c-En-dev.txt")
 
     # model = torch.load("30.model")
-    model = lstm_dy.Twitter.load("0")
+    model = lstm_dy.Twitter.load("29")
 
     word_index, char_index = utils.load_indices("indices")
     utils.make_vectors_test(tweets_test, word_index, char_index)
